e328223c5967305a1d2425b4c169386e85c908d6
guibsonblg <guibsonbrenner@hotmail.com>
2020-02-11 11:12:25 -0300

    Subindo backend

---
+++ src/index.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const http = require('http');
+const routes = require('./routes');
+const { setupWebsocket } = require('./websocket');
+
+const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
+
+mongoose.connect('mongodb+srv://omni10:omni10@cluster0-6t3ho.mongodb.net/devradar?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+
+server.listen(3333);
